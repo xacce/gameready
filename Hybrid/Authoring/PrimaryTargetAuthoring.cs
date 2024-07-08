b49abf2149de1d5fb95a24205bd64f705edc297d
Xacce <mmodbrreciever@gmail.com>
2024-07-09 00:47:24 +0300

    first

---
+++ Hybrid/Authoring/PrimaryTargetAuthoring.cs
@@ -0,0 +1,44 @@
+﻿#if UNITY_EDITOR
+using GameReady.Runtime;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace GameReady.Hybrid.Authoring
+{
+    public class PrimaryTargetAuthoring : MonoBehaviour
+    {
+        public GameObject setOnInit_s;
+
+        private class PrimaryTargetBaker : Baker<PrimaryTargetAuthoring>
+        {
+            public override void Bake(PrimaryTargetAuthoring authoring)
+            {
+                var e = GetEntity(TransformUsageFlags.None);
+                if (authoring.setOnInit_s != null)
+                {
+                    AddComponent(
+                        e,
+                        new PrimaryTarget()
+                        {
+                            target = GetEntity(authoring.setOnInit_s, TransformUsageFlags.WorldSpace),
+                            hitPosition = authoring.setOnInit_s.transform.position,
+                            angle = Vector3.Angle(authoring.transform.forward, math.normalize(authoring.setOnInit_s.transform.position - authoring.transform.position))
+                        });
+                }
+                else
+                {
+                    AddComponent(
+                        e,
+                        new PrimaryTarget()
+                        {
+
+                        });
+                }
+            }
+        }
+    }
+
+}
+
+#endif
---
+++ Runtime/Components.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace GameReady.Runtime
+{
+    public partial struct PrimaryTarget : IComponentData
+    {
+        public Entity target;
+        public float3 targetPosition;
+        public float3 hitPosition;
+        public float angle;
+        public bool hasTarget => !target.Equals(Entity.Null);
+    }
+
+    public partial struct Dead : IComponentData, IEnableableComponent
+    {
+        public float3 direction;
+    }
+}
